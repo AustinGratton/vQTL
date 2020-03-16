e31d23ff055584984510fc012e35317a5fb35d70
AustinGratton <grattonauz@gmail.com>
2020-03-16 19:29:28 -0400

    up to 1000 permutations

--- vQTL/SOV.perm/SOVperm.R
+++ vQTL/SOV.perm/SOVperm.R
@@ -8,6 +8,6 @@ library(tidyverse)
 setwd("/work/04902/azg5169/stampede2/vQTL/vQTL/SOV.perm")
 intResult = read_rds("InteractiveResult.rds")
 
-SOVperm = scanonevar.perm(intResult, n.perms = 2, random.seed = 3112020)
+SOVperm = scanonevar.perm(intResult, n.perms = 1000, random.seed = 3112020)
 
-write_rds(SOVperm, "permResult.rds")
+write_rds(SOVperm, "permResult1.rds")
--- vQTL/SOV.perm/batch_script.sh
+++ vQTL/SOV.perm/batch_script.sh
@@ -2,12 +2,12 @@
 #
 #
 #SBATCH -J minday
-#SBATCH -N 1
-#SBATCH -n 1
+#SBATCH -N 10
+#SBATCH -n 10
 #SBATCH --mail-user=azg5169@uncw.edu
 #SBATCH --mail-type=all 
 #SBATCH -p skx-normal
-#SBATCH -t 14:00:00
+#SBATCH -t 24:00:00
 #SBATCH -A TG-MCB180066
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
